b46b48fc8f5600a469c673a9c166d631d30364f5
flains <flains@ajou.ac.kr>
2026-05-20 16:02:05 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int solution(int a, int b) {
+    int answer = 0, la, lb;
+    char aa[10], bb[10], ab[10], ba[10];
+    
+    sprintf(aa, "%d", a), sprintf(bb, "%d", b);
+    
+    la = strlen(aa), lb = strlen(bb);
+    
+    strcpy(ab, aa);
+    strcpy(ab + la, bb);
+    
+    strcpy(ba, bb);
+    strcpy(ba + lb, aa);
+    
+    if(atoi(ab) >= atoi(ba))
+    {
+        answer = atoi(ab);
+    }
+    
+    else
+    {
+        answer = atoi(ba);
+    }
+    
+    return answer;
+}

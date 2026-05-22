cd7b5025f034a50db8bc63a81882692f40ac20c9
flains <flains@ajou.ac.kr>
2026-05-22 13:39:49 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.56 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int n, int k)
+{
+    int* answer = (int*)malloc((n / k) * sizeof(int));
+    int i = 0;
+    
+    for(i = 0; i < n / k; i++)
+    {
+        answer[i] = k * (i + 1);
+    }
+    
+    return answer;
+}

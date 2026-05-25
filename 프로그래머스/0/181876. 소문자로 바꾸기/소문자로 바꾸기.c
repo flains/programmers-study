ea4a2faff09762acca58e32ad052c24ed0481861
flains <flains@ajou.ac.kr>
2026-05-25 15:37:40 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(char* myString)
+{
+    for(int i = 0; i < strlen(myString); i++)
+        if(myString[i] <= 'Z') myString[i] += 32;
+
+    return myString;
+}

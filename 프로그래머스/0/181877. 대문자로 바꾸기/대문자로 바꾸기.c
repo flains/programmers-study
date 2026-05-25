99ed5bd58bfaa1b8cf307b0275676768fb021be5
flains <flains@ajou.ac.kr>
2026-05-25 15:36:12 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.c
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.c
@@ -5,7 +5,7 @@
 char* solution(char* myString)
 {
     for(int i = 0; i < strlen(myString); i++)
-        if('a' <= myString[i] && myString[i] <= 'z') myString[i] -= 32;
+        if('a' <= myString[i]) myString[i] -= 32;
 
     return myString;
 }

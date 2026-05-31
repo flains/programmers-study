800bd7b62c4924c961c70dea7c843b183afaf23b
flains <flains@ajou.ac.kr>
2026-06-01 03:10:26 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.25 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.c
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.c
@@ -2,10 +2,5 @@
 
 long long solution(int price, int money, int count)
 {
-    long long tot = 0;
-    for(int i = 0; i < count; i++)
-    {
-        tot += (i + 1) * price;
-    }
-    return (tot - money > 0) ? tot - money : 0;
+    return ((count + 1) * count / 2 * (long long)price - money > 0) ? (count + 1) * count / 2 * (long long)price - money : 0;
 }

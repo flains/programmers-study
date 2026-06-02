be5abb728bc8b8b30ded6efc04c78f0d60a8865f
flains <flains@ajou.ac.kr>
2026-06-02 13:58:49 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
@@ -2,5 +2,5 @@
 
 long long solution(int a, int b)
 {
-    return (b > a) ? (long long)(a + b) * (b - a + 1) / 2 : (long long)(a + b) * (a - b + 1) / 2;
+    return (long long)(a + b) * (abs(b - a) + 1) / 2;
 }

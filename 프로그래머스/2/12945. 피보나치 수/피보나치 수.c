ad5b34d8cf16b81443c13603ec7e22aee512b865
flains <flains@ajou.ac.kr>
2026-06-14 00:33:25 +0900

    [level 2] Title: 피보나치 수, Time: 0.31 ms, Memory: 3.95 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.c
@@ -0,0 +1,12 @@
+int solution(int n)
+{
+    int a = 0, b = 1, c, i;
+    
+    for(int i = 2; i <= n; i++)
+    {
+        c = (a + b) % 1234567;
+        a = b, b = c;
+    }
+    
+    return b;
+}

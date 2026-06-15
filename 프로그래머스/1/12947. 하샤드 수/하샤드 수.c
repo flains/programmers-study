56a19d983135b3f0eabb3b104acb4c2d28403e09
flains <flains@ajou.ac.kr>
2026-06-16 00:36:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool solution(int x)
+{
+    int i = 0, tot = 0, o = x;
+    for(i = 0; i < 7; i++)
+    {
+        tot += x % 10, x /= 10;
+    }
+    return (o % tot) ? false : true;
+}

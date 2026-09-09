490a9b8fafaecb039aee37f39bca65b9e4671f98
flains <flains@ajou.ac.kr>
2026-09-10 00:36:43 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 5.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.c
@@ -0,0 +1,11 @@
+int solution(int date1[], int date1_len, int date2[], int date2_len)
+{
+    if(date1[0] < date2[0]) return 1;
+    if(date1[0] > date2[0]) return 0;
+
+    if(date1[1] < date2[1]) return 1;
+    if(date1[1] > date2[1]) return 0;
+
+    if(date1[2] < date2[2]) return 1;
+    else return 0;
+}
